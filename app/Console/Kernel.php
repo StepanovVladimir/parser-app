39c67907070cf4b6b1ae4ca0cd6b7122e4c75dcc
StepanovVladimir <vovan.number.one98@gmail.com>
2021-07-31 12:24:31 +0300

    divided into functions

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -30,87 +30,97 @@ class Kernel extends ConsoleKernel
     {
         $schedule->call(function()
         {
-            $client = new Client();
-            $listPage = $client->request('GET', self::URL);
-
-            $newColleges = array();
-    
-            $parseItem = function($item) use (&$client, &$newColleges)
-            {
-                $title = $item->filter('.margin-top-none');
-                $name = $title->text();
-
-                $schoolImg = $item->filter('.school-image');
-                $schoolImgLarge = $item->filter('.school-image-large');
-                $image = $schoolImg->count() > 0
-                    ? $schoolImg->attr('src')
-                    : ($schoolImgLarge->count() > 0 ? $schoolImgLarge->attr('src') : null);
-    
-                [$city, $state] = explode(', ', $item->filter('.location')->text());
-    
-                $collegePage = $client->request('GET', $title->filter('a')->attr('href'));
-    
-                $schoolAddress = $collegePage->filter('.school-headline-address');
-                $address = $schoolAddress->filter('span')->text();
-                $site =$schoolAddress->filter('a')->attr('href');
-    
-                $phone = null;
-                $schoolContacts = $collegePage->filter('.school-contacts .col-xs-6');
-                for ($i = 0; $i < $schoolContacts->count(); $i++)
-                {
-                    if ($schoolContacts->eq($i)->text() == 'Phone')
-                    {
-                        $phone = $schoolContacts->eq($i + 1)->text();
-                        break;
-                    }
-                }
-    
-                $college = new College();
-                $college->name = $name;
-                $college->image = $image;
-                $college->state = $state;
-                $college->city = $city;
-                $college->address = $address;
-                $college->phone = $phone;
-                $college->site = $site;
-    
-                array_push($newColleges, $college);
-            };
-
-            $listPage->filter('.col-sm-height')->each($parseItem);
-            $listPage->filter('.vertical-padding')->each($parseItem);
-
-            $oldColleges = College::all();
-            foreach ($oldColleges as $college)
-            {
-                if (!$this->anyCollege($newColleges, $college))
-                {
-                    $college->delete();
-                }
-            }
-
-            foreach ($newColleges as $newCollege)
-            {
-                $oldCollege = College::firstWhere('name', $newCollege->name);
-                if (!$oldCollege)
-                {
-                    $newCollege->save();
-                }
-                else
-                {
-                    $oldCollege->name = $newCollege->name;
-                    $oldCollege->image = $newCollege->image;
-                    $oldCollege->state = $newCollege->state;
-                    $oldCollege->city = $newCollege->city;
-                    $oldCollege->address = $newCollege->address;
-                    $oldCollege->phone = $newCollege->phone;
-                    $oldCollege->site = $newCollege->site;
-
-                    $oldCollege->save();
-                }
-            }
-
+            $newColleges = $this->parseColleges();
+            $this->updateColleges($newColleges);
         })->hourly();
+    }
+
+    private function parseColleges(): array
+    {
+        $client = new Client();
+        $listPage = $client->request('GET', self::URL);
+
+        $colleges = array();
+    
+        $parseItem = function($item) use (&$client, &$colleges)
+        {
+            $title = $item->filter('.margin-top-none');
+            $name = $title->text();
+
+            $schoolImg = $item->filter('.school-image');
+            $schoolImgLarge = $item->filter('.school-image-large');
+            $image = $schoolImg->count() > 0
+                ? $schoolImg->attr('src')
+                : ($schoolImgLarge->count() > 0 ? $schoolImgLarge->attr('src') : null);
+
+            [$city, $state] = explode(', ', $item->filter('.location')->text());
+
+            $collegePage = $client->request('GET', $title->filter('a')->attr('href'));
+
+            $schoolAddress = $collegePage->filter('.school-headline-address');
+            $address = $schoolAddress->filter('span')->text();
+            $site =$schoolAddress->filter('a')->attr('href');
+
+            $phone = null;
+            $schoolContacts = $collegePage->filter('.school-contacts .col-xs-6');
+            for ($i = 0; $i < $schoolContacts->count(); $i++)
+            {
+                if ($schoolContacts->eq($i)->text() == 'Phone')
+                {
+                    $phone = $schoolContacts->eq($i + 1)->text();
+                    break;
+                }
+            }
+
+            $college = new College();
+            $college->name = $name;
+            $college->image = $image;
+            $college->state = $state;
+            $college->city = $city;
+            $college->address = $address;
+            $college->phone = $phone;
+            $college->site = $site;
+
+            array_push($colleges, $college);
+        };
+
+        $listPage->filter('.col-sm-height')->each($parseItem);
+        $listPage->filter('.vertical-padding')->each($parseItem);
+
+        return $colleges;
+    }
+
+    private function updateColleges(array $newColleges)
+    {
+        $oldColleges = College::all();
+        foreach ($oldColleges as $college)
+        {
+            if (!$this->anyCollege($newColleges, $college))
+            {
+                $college->delete();
+            }
+        }
+
+        foreach ($newColleges as $newCollege)
+        {
+            $oldCollege = College::firstWhere('name', $newCollege->name);
+            if (!$oldCollege)
+            {
+                $newCollege->save();
+            }
+            else
+            {
+                $oldCollege->name = $newCollege->name;
+                $oldCollege->image = $newCollege->image;
+                $oldCollege->state = $newCollege->state;
+                $oldCollege->city = $newCollege->city;
+                $oldCollege->address = $newCollege->address;
+                $oldCollege->phone = $newCollege->phone;
+                $oldCollege->site = $newCollege->site;
+
+                $oldCollege->save();
+            }
+        }
     }
 
     private function anyCollege(array $colleges, College $college): bool
